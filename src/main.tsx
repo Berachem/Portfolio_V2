339e81a226626b08e570254cb8847818f77b67f9
Berachem Markria <berachem.markria@gmail.com>
2024-12-07 21:42:22 +0100

    Analytics react

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import './index.css';
 import '../src/assets/App.css';
 
 import { ThemeProvider } from "@material-tailwind/react";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      
+      <Analytics />
     <App />
     </ThemeProvider>
   </StrictMode>
