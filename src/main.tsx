7a3c48812e361085e8dbe19693ada80bd2773d42
Berachem Markria <berachem.markria@gmail.com>
2024-12-07 23:46:39 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import '../src/assets/App.css';
 
 import { ThemeProvider } from '@material-tailwind/react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+/* import { SpeedInsights } from '@vercel/speed-insights/next'; */
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider>
             <Analytics />
-            <SpeedInsights />
+            {/*   <SpeedInsights /> */}
             <App />
         </ThemeProvider>
     </StrictMode>
