40bb66a0f2ab44f0190d67d92b3429592ea285f1
Berachem Markria <berachem.markria@gmail.com>
2024-12-07 23:44:19 +0100

    SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import App from './App.tsx';
 import './index.css';
 import '../src/assets/App.css';
 
-import { ThemeProvider } from "@material-tailwind/react";
-import { Analytics } from "@vercel/analytics/react"
+import { ThemeProvider } from '@material-tailwind/react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <Analytics />
-    <App />
-    </ThemeProvider>
-  </StrictMode>
+    <StrictMode>
+        <ThemeProvider>
+            <Analytics />
+            <SpeedInsights />
+            <App />
+        </ThemeProvider>
+    </StrictMode>
 );
